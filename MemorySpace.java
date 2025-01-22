300815cc3b1e136510c77dc250742bb76980f1b4
OrDavidovitz <Oriko8989@gmail.com>
2025-01-22 17:37:39 +0200

    hw9 now inished

--- MemorySpace.java
+++ MemorySpace.java
@@ -1,34 +1,13 @@
-/**
- * Represents a managed memory space. The memory space manages a list of allocated 
- * memory blocks, and a list free memory blocks. The methods "malloc" and "free" are 
- * used, respectively, for creating new blocks and recycling existing blocks.
- */
 public class MemorySpace {
-
-    // A list of the memory blocks that are presently allocated
+    private LinkedList freeList;
     private LinkedList allocatedList;
 
-    // A list of memory blocks that are presently free
-    private LinkedList freeList;
-
-    /**
-     * Constructs a new managed memory space of a given maximal size.
-     * 
-     * @param maxSize the size of the memory space to be managed
-     */
     public MemorySpace(int maxSize) {
-        allocatedList = new LinkedList();
         freeList = new LinkedList();
+        allocatedList = new LinkedList();
         freeList.addLast(new MemoryBlock(0, maxSize));
     }
 
-    /**
-     * Allocates a memory block of a requested length (in words). Returns the
-     * base address of the allocated block, or -1 if unable to allocate.
-     * 
-     * @param length the length (in words) of the memory block that has to be allocated
-     * @return the base address of the allocated block, or -1 if unable to allocate
-     */
     public int malloc(int length) {
         if (length <= 0) {
             throw new IllegalArgumentException("Invalid memory block size.");
@@ -47,15 +26,14 @@ public class MemorySpace {
                     if (previous == null) {
                         freeList.remove(0);
                     } else {
-                        freeList.remove(block);
+                        previous.next = current.next;
                     }
                 } else {
                     block.baseAddress += length;
                     block.length -= length;
                 }
 
-                MemoryBlock newBlock = new MemoryBlock(originalBase, length);
-                allocatedList.addLast(newBlock);
+                allocatedList.addLast(new MemoryBlock(originalBase, length));
                 return originalBase;
             }
 
@@ -66,11 +44,6 @@ public class MemorySpace {
         return -1;
     }
 
-    /**
-     * Frees the memory block whose base address equals the given address.
-     * 
-     * @param address the starting address of the block to free
-     */
     public void free(int address) {
         Node current = allocatedList.getFirst();
         Node previous = null;
@@ -82,7 +55,7 @@ public class MemorySpace {
                 if (previous == null) {
                     allocatedList.remove(0);
                 } else {
-                    allocatedList.remove(block);
+                    previous.next = current.next;
                 }
 
                 freeList.addLast(block);
@@ -96,18 +69,6 @@ public class MemorySpace {
         throw new IllegalArgumentException("Memory block with base address " + address + " not found in allocated list.");
     }
 
-    /**
-     * A textual representation of the free list and the allocated list of this memory space.
-     * 
-     * @return a string representation of the memory space
-     */
-    public String toString() {
-        return freeList.toString() + "\n" + allocatedList.toString();
-    }
-
-    /**
-     * Performs defragmentation of this memory space.
-     */
     public void defrag() {
         Node current = freeList.getFirst();
 
@@ -123,5 +84,9 @@ public class MemorySpace {
             }
         }
     }
-}
 
+    @Override
+    public String toString() {
+        return freeList.toString() + "\n" + allocatedList.toString();
+    }
+}
